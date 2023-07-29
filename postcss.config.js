0346fe0703f310eace5b9d45586a295289963f54
surazgaundel <gaundelsuraz99@gmail.com>
2023-07-29 20:33:17 +0530

    Post css file changed

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,5 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    'postcss-preset-env': {
-      features: { 'nesting-rules': true },
-    },
   },
 }
